19655dfe1061712f917e4940b4ecd2c367c5a5f9
Abdullah <hafiz.abdullah.hashmi@gmail.com>
2026-06-09 04:07:21 +0500

    Build compiled production assets for login pages

--- public/build/assets/arrow-down-left-Bl_paHrW.js
+++
@@ -1,1 +0,0 @@
-import{e as o}from"./app-C-gfzgLh.js";const e=[["path",{d:"M17 7 7 17",key:"15tmo1"}],["path",{d:"M17 17H7V7",key:"1org7z"}]],r=o("arrow-down-left",e);export{r as A};
---
+++ public/build/assets/minus-DAhhZOPQ.js
@@ -0,0 +1,1 @@
+import{e as o}from"./app-NrP7oK3N.js";const e=[["path",{d:"M5 12h14",key:"1ays0h"}]],c=o("minus",e);export{c as M};
