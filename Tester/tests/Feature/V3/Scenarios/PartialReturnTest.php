11a33afb49300e5e203325d135c68a7e634509e9
Abdullah Hashmi (Fable5 session) <ovntec695@gmail.com>
2026-07-11 09:15:16 +0500

    test: align sales schema in PartialReturnTest mock setup
    Removed the deprecated journal_entry_id column from the sales table mock insert.

--- Tester/tests/Feature/V3/Scenarios/PartialReturnTest.php
+++ Tester/tests/Feature/V3/Scenarios/PartialReturnTest.php
@@ -168,7 +168,6 @@ class PartialReturnTest extends TestCase
             'payment_method'   => $paymentMethod,
             'reference_number' => 'SAL-' . Str::random(4),
             'status'           => 'posted',
-            'journal_entry_id' => $jeId,
             'subtotal'         => $grossTotal,
             'total'            => $netTotal,
             'net_sales'        => $netTotal,
